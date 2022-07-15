0a97068047e347f462a3b14f8afbf2a1c356d981
Masooma <masooma.zahedi1@gmail.com>
2022-07-15 14:16:40 -0500

    Fixed Print Part

--- src/sitePart/component/MainContent.js
+++ src/sitePart/component/MainContent.js
@@ -2,7 +2,7 @@ import React, {useRef} from "react";
 import { useReactToPrint } from 'react-to-print';
 
 
-const MainContent = ({infoItems})=>{
+const MainContent = ({infoItems, innerImgIframePrint})=>{
 
     const componentRef = useRef();
     const handlePrint2 = useReactToPrint({
@@ -14,14 +14,40 @@ const MainContent = ({infoItems})=>{
         document.getElementById("remove3").innerHTML ='';
 
         handlePrint2();
-        document.getElementById("remove2").innerHTML = '<img  src="https://www.gardeningknowhow.com/wp-content/uploads/2021/09/Columnar-Apple-400x533.jpg" alt="" />'
-        document.getElementById("remove3").innerHTML ='<iframe className="d-block bg-warning mx-auto " width="760" height="415" src="https://www.youtube.com/embed/g3hTu-iqYuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';        
+        // document.getElementById("remove2").innerHTML = '<img  src="https://www.gardeningknowhow.com/wp-content/uploads/2021/09/Columnar-Apple-400x533.jpg" alt="" />'
+        // document.getElementById("remove3").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Fj430mbb_20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        innerImgIframePrint()
     }
 
 
     return(
         <>
                 <div className="col-9 px-4" ref= {componentRef}>
+                    <h1>{infoItems.topTitle} </h1>
+                    <p>{infoItems.auther}</p>
+                    <div  id="remove1">
+                        <button onClick={handlePrint}  className='noPrint'>
+                            <i className='fa fa-print' ></i> Printer Friendly Version
+                        </button>
+                    </div>
+                    <div className="row" >
+                        <div className="col-7 px-4" >
+                            {infoItems.describe}
+                        </div>
+                        <div className="col-5 noPrint" id="remove2" >
+                            <img  src={infoItems.srcImg} alt="" />
+                        </div>
+                        <h2 >What is {infoItems.questoin} </h2>
+                        <p > {infoItems.answerQ} </p>
+                        <div className=' border noPrint' id="remove3" >
+                            {infoItems.iframe}
+                        </div>
+                        <p>{infoItems.descIframe}</p>
+                        <h2>{infoItems.title1} </h2>
+                        <p>{infoItems.infoTitle1} </p>
+                    </div>
+                </div>                
+                {/* <div className="col-9 px-4" ref= {componentRef}>
                     <h1>{infoItems.appleTree[0].topTitle} </h1>
                     <p>{infoItems.appleTree[0].auther}</p>
                     <div  id="remove1">
@@ -45,7 +71,7 @@ const MainContent = ({infoItems})=>{
                         <h2>{infoItems.appleTree[0].title1} </h2>
                         <p>{infoItems.appleTree[0].infoTitle1} </p>
                     </div>
-                </div>                
+                </div>                 */}
         </>
     )
 }
--- src/sitePart/pages/gardenPage/subPageGarden/ediblePage/subEdiblePage/FruitsPage.js
+++ src/sitePart/pages/gardenPage/subPageGarden/ediblePage/subEdiblePage/FruitsPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { useReactToPrint } from 'react-to-print';
 import { Link, Route, Routes} from "react-router-dom";
 import { RightSidePart } from '../../../../../component/RightSidePart';
 import { TopTitle } from '../../../../../component/subTabMenu/TopPartSubTab';
@@ -6,10 +7,26 @@ import {Section3, Section4} from '../../../../../component/SectionsBottomPage';
 import MainContent from '../../../../../component/MainContent';
 
 // *********************** Data for Page************************************
+        // {
+        //     mainTitle:"",
+        //     topTitle:"",
+        //     auther:"",
+        //     describe:"",
+        //     srcImg:"",
+        //     questoin:"",
+        //     answerQ:"",
+        //     iframe:"",
+        //     descIframe:"",
+        //     title1:"",
+        //     infoTitle1:"",
+
+        // }
+// *************************************************************************
 
 const infoItems = {
     appleTree:[
-        {
+        {   
+            mainTitle:"Apple Tree",
             topTitle:"What Is A Columnar Apple Tree – Columnar Apple Tree Varieties",
             auther:"By: Teo Spengler",
             describe:"What is a columnar apple tree? These are unusually narrow and upright-growing apple trees, tall and thin and perfect for small spaces. You can grow them in a garden corner or in a container on the patio. Although columnar apple tree varieties (Malus domesticus cvs.) look quite different from regular apple trees, columnar apple fruit looks like regular apples. Read on if you are interested in growing columnar apple trees.",
@@ -22,9 +39,25 @@ const infoItems = {
             infoTitle1:"If you are thinking of growing columnar apple trees, you will want to know the ins and outs of planting. We have lots of tips about how to grow columnar trees, but only try this if you live in U.S. Department of Agriculture plant hardiness zones 4 through 9. First, columnar trees are fruit trees, despite their unusual shape. Like regular apple trees, they require a full sun location to thrive. You’ll need at least two different columnar apple tree varieties for pollination. If you plant them in the soil of your backyard, space them about 2 feet (61 cm.). Planting them in large containers or whiskey barrels works just fine. Whether they are in containers or in the ground, you’ll need to water them regularly if you want columnar apple fruit.",
         
         }
+    ],
+    galaAppleTree:[
+        {
+            mainTitle:"Gala Apple",
+            topTitle:"Gala Apple Tree Care – How To Grow A Gala Apple Fruit Tree",
+            auther:"By: Teo Spengler",
+            describe:"There aren’t many fruit trees that can keep you in food most of the year. The Gala apple fruit tree is one of them. Famed for their crispness, Gala apples can be harvested most of the autumn and can be stored for months while retaining an excellent texture. If you are thinking of growing a Gala apple tree, who can blame you? Read on for tips to make Gala apple tree care as easy as possible.",
+            srcImg:"https://www.gardeningknowhow.com/wp-content/uploads/2021/07/gala-apple-400x300.jpg",
+            questoin:"Gala Apple Fruit Tree",
+            answerQ:"Most apple trees dress up like ballerinas in their spring blossoms, and planting a Gala apple tree brings this show into your own backyard. Its flowers are white-pink and fragrant, giving way to young fruit in summer.As the Gala apples mature, they become ornamental too, developing attractive streaks of yellow on their firm surfaces. The harvest period is long, lasting all through autumn, and that’s when you get to taste the sweet, firm, and juicy fruit.",
+            iframe:<iframe width="560" height="315" src="https://www.youtube.com/embed/Fj430mbb_20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+            descIframe:"Like every other plant, the Gala apple grows best when its particular needs are met. The ideal Gala apple tree climate is not-too-warm and not-too cool. It thrives in the middle zones, USDA plant hardiness zones 5 through 8. When planting a Gala apple tree, find a site that gets full, direct sun. It needs at least six hours of unfiltered sun per day. Excellent drainage is equally important so provide it with moist, well-drained soil. Note that the tree is self-pollinating, so you don’t need more than one. Two apple trees are always better though.",
+            title1:"Gala Apple Tree Care",
+            infoTitle1:"Once you’ve got Gala apple tree climate figured out, you are ready to plant. Either fall or spring works well for this tree but avoid very cold or very hot periods. The amount of garden space you’ll need for the tree depends on which version you pick. The standard Gala grows to 25 feet (8.5 m.) tall with a similar spread, the semi-dwarf is about half that tall, while a dwarf stays about 10 feet (3 m.) tall and wide.  Gala apple fruit trees need ample water for the first year. Irrigate deeply at planting time, then twice weekly for a few months while the tree is establishing. One deep watering weekly during the growing season works well the first year, with one monthly in winter. After establishment, water only during dry spells. Mulching helps keep the soil moist.  What about fertilizer? Apply it when planting a Gala apple tree, and again each spring. Prune the tree in late winter to take out dead, diseased, or crowded branches.",
+            
+        }
     ]
 }
-// ========>> formula for Apple Tree <<=====================================
+// ========>> for Apple Tree <<=====================================
 function AppleTree (props){
 
     const addTitle = ()=>{
@@ -43,14 +76,13 @@ function AppleTree (props){
           &nbsp;›
         </span>
         <span style={{ color: "rgb(87 161 195)" }}>
-          &nbsp;Apple Tree
+          &nbsp;{infoItems.appleTree[0].mainTitle}
         </span>
-        {/* //// */}
       </>
       )
       }
 
-    //   information => top Title ///////////
+    //  +++++++++++++++++++++++++++ information => top Title ++++++++++++++++++++++++++++
     const appleTree = {
         topInfo:[
             {
@@ -61,10 +93,16 @@ function AppleTree (props){
             },
         ]
     }
+// +++++++++++++++++++++++++++++++++++++ Print Page ++++++++++++++++++++++++++++====
+ 
+const innerImgIframePrint = () =>{
+    return (
+        document.getElementById("remove2").innerHTML = '<img  src="https://www.gardeningknowhow.com/wp-content/uploads/2021/09/Columnar-Apple-400x533.jpg" alt="" />',
+        document.getElementById("remove3").innerHTML = '<iframe className="d-block bg-warning mx-auto " width="760" height="415" src="https://www.youtube.com/embed/g3hTu-iqYuE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        )
+}
 
-
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+// ++++++++++++++++++++++++++++++++++++++++++++  Apple Tree  +++++++++++++++++++++++++++++=
     return(
         <>
             <div className="container" >
@@ -74,7 +112,7 @@ function AppleTree (props){
                 </div>
                 <div className="subinfo mb-5 row">
                     {/* start main for apple tree */}
-                    <MainContent infoItems={infoItems}/>
+                    <MainContent infoItems={infoItems.appleTree[0]} innerImgIframePrint={innerImgIframePrint} />
                     {/* end of main for apple tree */}
                     <div className="col-3 border"><RightSidePart /></div>
                 </div>
@@ -106,15 +144,89 @@ function AppleTree (props){
 
 
 
-
+// ******************************************* Gala Apple Tree ******************
 
 function Apple (){
+    const addTitle = ()=>{
+        return(
+        <>
+          <span className="" style={{ color: "rgb(51 51 51)" }}>
+          &nbsp;›
+        </span>
+        <Link to="/gardens/edible/fruits" style={{textDecoration:"none"}}>
+            <span style={{ color: "rgb(87 161 195)" }}>
+            &nbsp;Fruits
+            </span>
+        </Link>
+        {/* //// */}
+        <span className="" style={{ color: "rgb(51 51 51)" }}>
+          &nbsp;›
+        </span>
+        <span style={{ color: "rgb(87 161 195)" }}>
+          &nbsp;{infoItems.galaAppleTree[0].mainTitle}
+        </span>
+        {/* //// */}
+      </>
+      )
+      }
+
+    //   information => top Title ///////////
+    const appleTree = {
+        topInfo:[
+            {
+                title1: "Gardens",
+                linkTo1:"/gardens",
+                title2: "Edible",
+                linkTo2:"/gardens/edible"
+            },
+        ]
+    }
+    // ********************************** Print Part ************************
+    const innerImgIframePrint = () =>{
+        return (
+            document.getElementById("remove2").innerHTML = '<img  src="https://www.gardeningknowhow.com/wp-content/uploads/2021/07/gala-apple-400x300.jpg" alt="" />',
+            document.getElementById("remove3").innerHTML =' <iframe width="560" height="315" src="https://www.youtube.com/embed/Fj430mbb_20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+            )
+    }
+    
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
     return(
         <>
-            <h1>Hi masooma Apple</h1>        
+            <div className="container" >
+                <div className="mt-5">
+                <TopTitle dataTop={appleTree} addTitle={addTitle()}/>
+
+                </div>
+                <div className="subinfo mb-5 row">
+                    {/* start main for apple tree */}
+                    <MainContent infoItems={infoItems.galaAppleTree[0]} innerImgIframePrint={innerImgIframePrint} />
+                    {/* end of main for apple tree */}
+                    <div className="col-3 border"><RightSidePart /></div>
+                </div>
+                <div className='pt-5'>
+                    <Section3 />
+                </div>
+            </div>
+            <Section4/>
         </>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function Banana (){
     return(
         <>
