fd3bc9a5d30ca212bce727922cda9fb0d7720452
Masooma <masooma.zahedi1@gmail.com>
2022-09-15 11:22:49 -0500

    complete onamental page

--- src/sitePart/pages/gardenPage/subPageGarden/OrnamentalPage/CactiOrnamental.js
+++ src/sitePart/pages/gardenPage/subPageGarden/OrnamentalPage/CactiOrnamental.js
@@ -1,11 +1,134 @@
+import React from 'react';
+import { RightSidePart } from '../../../../component/RightSidePart';
+import { TopTitle } from '../../../../component/subTabMenu/TopPartSubTab';
+import {Section3, Section4} from '../../../../component/SectionsBottomPage';
+import MainContent from '../../../../component/MainContent';
+
+// *********************** Data for Page************************************
+        // {
+        //     mainTitle:"",
+        //     topTitle:"",
+        //     auther:"",
+        //     describe:"",
+        //     srcImg:"",
+        //     questoin:"",
+        //     answerQ:"",
+        //     iframe:"",
+        //     descIframe:"",
+        //     title1:"",
+        //     infoTitle1:"",
+        // }
+// *************************************************************************
+
+const infoItems = {
+    crassula   :[
+        {
+            mainTitle:"CactiOrnamental",
+            topTitle:"Crassula Pagoda Plants: How To Grow Red Pagoda Crassula Plant",
+            auther:"By: Bonnie L. Grant, Certified Urban Agriculturist",
+            describe:"Collectors of succulents will be excited about Crassula pagoda plants. For sheer architectural interest, this unique plant evokes images of a trip to Shanghai where religious temples reflect the unimaginable forms of stacked architecture and astounding building prowess. Red Pagoda Crassula is an easy-to-grow plant that will add punch to any succulent display or as a stand-alone specimen. Here are a few tips on how to grow Red Pagoda and harness the impact of its structure and colorful foliage.",
+            srcImg:"https://www.gardeningknowhow.com/wp-content/uploads/2021/01/crassula-exotic-succulent-400x300.jpg",
+            questoin:"Red Pagoda Crassula ? ",
+            answerQ:"Succulents offer a wide range of form to suit any discernible gardener. The Crassula genus has over 150 different species, each more surprising than the last. The fairly common jade plant is in the genus. Crassula pagoda plants have an even more diverse “wow” factor. The stacked triangular foliage edged with red and fine prickly spines is eye candy for the succulent enthusiast. Growing Crassula Red Pagoda is a must for the gardener crazy about succulents and collecting.",
+            iframe:<iframe width="560" height="315" src="https://www.youtube.com/embed/qxJ4DrcDZV8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+            descIframe:"Red Pagoda (Crassula corymbulosa) grows spirally arranged, with thick, fleshy leaves bordered with bright pink, red, or sometimes orange. Once you get over the color, the geometric arrangement of the leaves will astound and amaze you. The effect of the layered leaves is hard to describe without invoking artistic expression. ",
+            title1:"How to Grow Red Pagoda",
+            infoTitle1:"Soil, light, and air circulation are key needs for growing Crassula Red Pagoda. The stems are initially erect but over time, as new rosettes form, they will begin to trail. This means you can grow the plant in a hanging basket. It is equally at home in a rockery, clay pot, or amongst a display of other succulents in the garden. Red Pagoda is only hardy in United States Department of Agriculture zones 11 to 12 but it performs well as a houseplant, too. Plants prefer well-drained soil with plenty of grit but can survive in amended clay soil. Like most succulents, Red Pagoda is easy to grow from cuttings. Allow the cutting to callus for a few days and insert into a soilless medium. In a month or so, the plant will root and can be transplanted to a display container or the garden.",
+        }
+    ],
+
+}
+
+// ==========================================================================================
+// +++++++++++++++++++++++++++++++++++++++++ Pollinator ++++++++++++++++++++++++++++++++
+function Crassula (props){
+
+    const addTitle = ()=>{
+        return(
+        <>
+        <span className="" style={{ color: "rgb(51 51 51)" }}>
+          &nbsp;›
+        </span>
+        <span style={{ color: "rgb(87 161 195)" }}>
+          &nbsp;{infoItems.crassula [0].mainTitle}
+        </span>
+      </>
+      )
+      }
+
+    //  +++++++++++++++++++++++++++ information => top Title ++++++++++++++++++++++++++++
+    const crassula  = {
+        topInfo:[
+            {
+                title1: "Gardens",
+                linkTo1:"/gardens",
+                title2: "Ornamental",
+                linkTo2:"/gardens/ornamental"
+            },
+        ]
+    }
+// +++++++++++++++++++++++++++++++++++++ Print Page ++++++++++++++++++++++++++++====
+ 
+    const innerImgIframePrint = () =>{
+        return (
+            document.getElementById("remove2").innerHTML = '<img src="https://www.gardeningknowhow.com/wp-content/uploads/2021/01/crassula-exotic-succulent-400x300.jpg"/>',
+            document.getElementById("remove3").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/qxJ4DrcDZV8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+            )
+    }
+
+// +++++++++++++++++++++++++++++++++++++ Main Return++++++++++++++++++++++++++=
+    return(
+        <>
+            <div className="container" >
+                <div className="mt-5">
+                <TopTitle dataTop={crassula } addTitle={addTitle()}/>
+
+                </div>
+                <div className="subinfo mb-5 row">
+                    {/* start main for apple tree */}
+                    <MainContent infoItems={infoItems.crassula [0]} innerImgIframePrint={innerImgIframePrint} />
+                    {/* end of main for apple tree */}
+                    <div className="col-3 border"><RightSidePart /></div>
+                </div>
+                <div className='pt-5'>
+                    <Section3 />
+                </div>
+            </div>
+            <Section4/>
+        </>
+    )
+}
+
+
+// ==================================================================================
+
+
+
 function CactiOrnamental(params) {
     return(
         <>
-            <h1>
-            CactiOrnamental Page like Howto Page
-            </h1>
+            <Crassula />
         </>
     )
 }
 
 export default CactiOrnamental;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/sitePart/pages/gardenPage/subPageGarden/OrnamentalPage/FungusOrnamental.js
+++ src/sitePart/pages/gardenPage/subPageGarden/OrnamentalPage/FungusOrnamental.js
@@ -1,11 +1,137 @@
+import React from 'react';
+import { RightSidePart } from '../../../../component/RightSidePart';
+import { TopTitle } from '../../../../component/subTabMenu/TopPartSubTab';
+import {Section3, Section4} from '../../../../component/SectionsBottomPage';
+import MainContent from '../../../../component/MainContent';
+
+// *********************** Data for Page************************************
+        // {
+        //     mainTitle:"",
+        //     topTitle:"",
+        //     auther:"",
+        //     describe:"",
+        //     srcImg:"",
+        //     questoin:"",
+        //     answerQ:"",
+        //     iframe:"",
+        //     descIframe:"",
+        //     title1:"",
+        //     infoTitle1:"",
+        // }
+// *************************************************************************
+
+const infoItems = {
+    fungi :[
+        {
+            mainTitle:"FungusOrnamental",
+            topTitle:"Environmental Benefits Of Fungi: Are Mushrooms Good For The Environment ",
+            auther:"By: Mary Ellen Ellis",
+            describe:"Are mushrooms good for the environment? Fungi are often associated with unwanted growth or even health problems. Molds, fungal infections, and poisonous mushrooms are certainly sinister. However, mushrooms and fungi have a place in the ecosystem and many types have important environmental benefits.",
+            srcImg:"https://www.gardeningknowhow.com/wp-content/uploads/2020/07/mushrooms-400x300.jpg",
+            questoin:"Environmental Benefits of Fungi ?",
+            answerQ:"Fungi and mushroom benefits in the environment are huge. Without them, dead plant and animal matter would pile up and decay much more slowly. Fungi are essential for processing dead material, healthy plant growth, nutrition, medicine, and for the entire rise of animal life on earth as well as human civilizations.",
+            iframe:<iframe width="560" height="315" src="https://www.youtube.com/embed/8kxoEQy57Bw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+            descIframe:"Saprophytes: These are the fungi that recycle nutrients. They break organic matter down to create rich soil in which plants thrive. Bacteria and insects help the process, but the saprophyte fungi are responsible for most of the nutrient cycling that supports life on earth. Mycorrhizae: This type of fungi is also important for plant growth. They produce long, thin filaments in the soil that connect roots to create a symbiotic network. They take nutrients from plants, like trees, but also provide water and nutrients to the roots. Plants with mycorrhizae fungi thrive compared to those without them. Edible and Medicinal fungi: Many species of fungi are edible and provide essential nutrients for many animals. Caribou, for instance, eat lichen in the winter when plant life is not available. Without that fungi, they couldn’t survive. For humans, many edible mushrooms provide nutrients and health benefits. Some even have medicinal properties and can boost immunity, guard against inflammation, and treat infections. Penicillin came from mold after all.",
+            title1:"Environmentally Friendly Fungi ",
+            infoTitle1:"Fungal growth offers a cheap, simple and environmentally friendly way to bind agricultural byproducts (such as rice hulls, wheat straw, sugarcane bagasse and molasses) into biodegradable and carbon-neutral foams.Fungal foams are becoming increasingly popular as sustainable packaging materials; IKEA is one company that has indicated a commitment to using them.Fungal foams can also be used in the construction industry for insulation, flooring and panelling. Research has revealed them to be strong competitors against commercial materials in terms of having effective sound and heat insulation properties.",
+        }
+    ],
+
+}
+
+// ==========================================================================================
+// +++++++++++++++++++++++++++++++++++++++++ Pollinator ++++++++++++++++++++++++++++++++
+function Fungi (props){
+
+    const addTitle = ()=>{
+        return(
+        <>
+        <span className="" style={{ color: "rgb(51 51 51)" }}>
+          &nbsp;›
+        </span>
+        <span style={{ color: "rgb(87 161 195)" }}>
+          &nbsp;{infoItems.fungi [0].mainTitle}
+        </span>
+      </>
+      )
+      }
+
+    //  +++++++++++++++++++++++++++ information => top Title ++++++++++++++++++++++++++++
+    const fungi  = {
+        topInfo:[
+            {
+                title1: "Gardens",
+                linkTo1:"/gardens",
+                title2: "Ornamental",
+                linkTo2:"/gardens/ornamental"
+            },
+        ]
+    }
+// +++++++++++++++++++++++++++++++++++++ Print Page ++++++++++++++++++++++++++++====
+ 
+    const innerImgIframePrint = () =>{
+        return (
+            document.getElementById("remove2").innerHTML = '<img src="https://www.gardeningknowhow.com/wp-content/uploads/2020/07/mushrooms-400x300.jpg"/>',
+            document.getElementById("remove3").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/8kxoEQy57Bw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+            )
+    }
+
+// +++++++++++++++++++++++++++++++++++++ Main Return++++++++++++++++++++++++++=
+    return(
+        <>
+            <div className="container" >
+                <div className="mt-5">
+                <TopTitle dataTop={fungi } addTitle={addTitle()}/>
+
+                </div>
+                <div className="subinfo mb-5 row">
+                    {/* start main for apple tree */}
+                    <MainContent infoItems={infoItems.fungi [0]} innerImgIframePrint={innerImgIframePrint} />
+                    {/* end of main for apple tree */}
+                    <div className="col-3 border"><RightSidePart /></div>
+                </div>
+                <div className='pt-5'>
+                    <Section3 />
+                </div>
+            </div>
+            <Section4/>
+        </>
+    )
+}
+
+
+// ==================================================================================
+
+
+
 function FungusOrnamental(params) {
     return(
         <>
-            <h1>
-            FungusOrnamental Page like Howto Page
-            </h1>
+            <Fungi />
         </>
     )
 }
 
 export default FungusOrnamental;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
